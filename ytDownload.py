9d779ee9f03ddddea2f24390e44ec0ee4370ca4f
kevinSteiger <kevinnsteiger@gmail.com>
2023-07-26 23:07:13 -0400

    Update ytDownload.py
    Set up video downloading with link input and itag choice. Having issues with audio.

--- ytDownload.py
+++ ytDownload.py
@@ -1,11 +1,23 @@
 from pytube import YouTube
 
+#HD test Video linK: https://www.youtube.com/watch?v=6pxRHBw-k8M
+#Normal test Video Link: https://www.youtube.com/watch?v=v3UBlEJDXR0
 
-vid = YouTube("https://www.youtube.com/watch?v=v3UBlEJDXR0")
+
+link = input("Paste Video Link: ")
+
+vid = YouTube(link)
 
 title = vid.title
 
-print("You will now be downloading: " + title)
-vid.streams.filter(progressive=True)
+print(vid.streams.filter(adaptive=True))
+itag = input("\nPlease select an itag from above based on the video quality/type you'd like: ")
+print("\nYou will now be downloading: " + title)
 
-vid.download(output_path = r"c:\Users\kev1n\Downloads", filename = title)
+
+#stream = vid.streams.first()
+
+stream = vid.streams.get_by_itag(itag)
+print("Downloading...this may take a while")
+stream.download(output_path = r"c:\Users\kev1n\Downloads", filename = title)
+print("End of program")
