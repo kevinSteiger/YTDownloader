becb7205beb8750c4222724a92f34191f8eeefa2
kevinSteiger <kevinnsteiger@gmail.com>
2023-07-26 22:27:59 -0400

    Update ytDownload.py

--- ytDownload.py
+++ ytDownload.py
@@ -6,6 +6,6 @@ vid = YouTube("https://www.youtube.com/watch?v=v3UBlEJDXR0")
 title = vid.title
 
 print("You will now be downloading: " + title)
-#vid.streams.filter(progressive=True)
+vid.streams.filter(progressive=True)
 
-#vid.download(output_path = r"c:\Users\kev1n\Downloads", filename = title)
+vid.download(output_path = r"c:\Users\kev1n\Downloads", filename = title)
