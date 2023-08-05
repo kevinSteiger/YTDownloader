203324ab040bef837fca3ffaf0a0d322a3ea7431
kevinSteiger <kevinnsteiger@gmail.com>
2023-08-05 19:48:10 -0400

    Update ytDownload.py

--- ytDownload.py
+++ ytDownload.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 import ffmpeg
+import subprocess
 
 
 #Problem: https://stackoverflow.com/questions/58456229/i-am-trying-to-download-video-using-pytube-however-for-some-reason-no-sound-is-d
@@ -12,7 +13,7 @@ title = vid.title
 
 print("You will now be downloading: " + title)
 
-video = vid.streams.get_by_itag(136)
+video = vid.streams.get_by_itag(313)
 audio = vid.streams.get_by_itag(140)
 
 print("Downloading...this may take a while")
@@ -23,10 +24,8 @@ audio.download(filename = title + "audio.mp4")
 
 videoStream = ffmpeg.input(title + "video.mp4")
 audioStream = ffmpeg.input(title + "audio.mp4")
+video = title + ".mp4"
 
-ffmpeg.output(audioStream, videoStream,'out_mp4').run()
-#out = ffmpeg.output(videoStream, audioStream, r"c:\Users\kev1n\Downloads", vcodec='copy', acodec='copy', strict='experimental')
-#out.run()
-
+ffmpeg.output(videoStream, audioStream, video, vcodec='copy', acodec='aac', strict='experimental', b='192k').run()
 
 print("End of program")
